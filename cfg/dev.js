a89c143e4c5f83ed2cf6dbad59a74a84b315c2be
ningmeng1989 <380215444@qq.com>
2016-10-29 14:46:47 +0800

    修复了原例子中，发布目录不能采用相对路径的问题，现改为发布和开发为单独的webpack配置文件

--- cfg/dev.js
+++ cfg/dev.js
@@ -2,15 +2,15 @@
 
 let path = require('path');
 let webpack = require('webpack');
-let baseConfig = require('./base');
-let defaultSettings = require('./defaults');
 
 // Add needed plugins here
 let BowerWebpackPlugin = require('bower-webpack-plugin');
+const defaultSrcPath = path.join(__dirname, '/../src');
+let additionalPaths = [];
 
-let config = Object.assign({}, baseConfig, {
+let config = {
   entry: [
-    'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
+    'webpack-dev-server/client?http://127.0.0.1:' + 8000,
     'webpack/hot/only-dev-server',
     './src/index'
   ],
@@ -23,17 +23,84 @@ let config = Object.assign({}, baseConfig, {
       searchResolveModulesDirectories: false
     })
   ],
-  module: defaultSettings.getDefaultModules()
-});
-
-// Add needed loaders to the defaults here
-config.module.loaders.push({
-  test: /\.(js|jsx)$/,
-  loader: 'react-hot!babel-loader',
-  include: [].concat(
-    config.additionalPaths,
-    [ path.join(__dirname, '/../src') ]
-  )
-});
+  additionalPaths: additionalPaths,
+  port: 8000,
+  debug: true,
+  output: {
+    path: path.join(__dirname, '/../dist'),
+    filename: 'app.js',
+    publicPath: '/assets/'
+  },
+  devServer: {
+    contentBase: './src/',
+    historyApiFallback: true,
+    hot: true,
+    port: 8000,
+    publicPath: '/assets/',
+    noInfo: false
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      actions: `${defaultSrcPath}/actions/`,
+      components: `${defaultSrcPath}/components/`,
+      sources: `${defaultSrcPath}/sources/`,
+      stores: `${defaultSrcPath}/stores/`,
+      styles: `${defaultSrcPath}/styles/`,
+      config: `${defaultSrcPath}/config/` + process.env.REACT_WEBPACK_ENV
+    }
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        include: defaultSrcPath,
+        loader: 'eslint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}'
+      },
+      {
+        test: /\.sass/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!sass-loader?outputStyle=expanded'
+      },
+      {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!less-loader'
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!stylus-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.(mp4|ogg|svg)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'react-hot!babel-loader',
+        include: [].concat(
+          additionalPaths,
+          [ path.join(__dirname, '/../src') ]
+        )
+      }
+    ]
+  }
+};
 
 module.exports = config;
--- cfg/dist.js
+++ cfg/dist.js
@@ -3,16 +3,20 @@
 let path = require('path');
 let webpack = require('webpack');
 
-let baseConfig = require('./base');
-let defaultSettings = require('./defaults');
-
 // Add needed plugins here
 let BowerWebpackPlugin = require('bower-webpack-plugin');
+let additionalPaths = [];
+const defaultSrcPath = path.join(__dirname, '/../src');
 
-let config = Object.assign({}, baseConfig, {
+let config = {
   entry: path.join(__dirname, '../src/index'),
+  output: {
+    path: 'dist/assets',
+    filename: 'app.js',
+    publicPath: 'assets/'
+  },
   cache: false,
-  devtool: 'sourcemap',
+  // devtool: 'sourcemap',
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
@@ -26,17 +30,71 @@ let config = Object.assign({}, baseConfig, {
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.NoErrorsPlugin()
   ],
-  module: defaultSettings.getDefaultModules()
-});
-
-// Add needed loaders to the defaults here
-config.module.loaders.push({
-  test: /\.(js|jsx)$/,
-  loader: 'babel',
-  include: [].concat(
-    config.additionalPaths,
-    [ path.join(__dirname, '/../src') ]
-  )
-});
+  additionalPaths: additionalPaths,
+  port:  8000,
+  // debug: true,
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      actions: `${defaultSrcPath}/actions/`,
+      components: `${defaultSrcPath}/components/`,
+      sources: `${defaultSrcPath}/sources/`,
+      stores: `${defaultSrcPath}/stores/`,
+      styles: `${defaultSrcPath}/styles/`,
+      config: `${defaultSrcPath}/config/` + process.env.REACT_WEBPACK_ENV
+    }
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        include: defaultSrcPath,
+        loader: 'eslint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}'
+      },
+      {
+        test: /\.sass/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!sass-loader?outputStyle=expanded'
+      },
+      {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!less-loader'
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!stylus-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.(mp4|ogg|svg)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel',
+        include: [].concat(
+          additionalPaths,
+          [ path.join(__dirname, '/../src') ]
+        )
+      }
+    ]
+  }
+};
 
 module.exports = config;
